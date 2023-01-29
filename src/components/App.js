9be8e22f4b1ea509e9146e0b50aea5b4b682b639
KarimCarl <cndungu109@gmail.com>
2023-01-29 19:19:03 +0300

    Updated div to fragments

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PetContainer from '../containers/SearchPetContainer';
 
 const App = () => (
-  <div>
+  <>
     <PetContainer />
-  </div>
+  </>
 );
 
 export default App;
